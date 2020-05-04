65f9b25e28b49e500cd4783a1ad000c58019602a
miguelpan <miguel_tobi@hotmail.com>
2020-05-04 02:49:01 -0300

    Editando formulario de cadastro de seller e dando upgrade no metodo update seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -4,8 +4,11 @@ package gui;
  * Controller do SellerForm
  */
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -20,6 +23,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -47,7 +51,25 @@ public class SellerFormController implements Initializable {
 	private TextField txtName;
 
 	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	private DatePicker dpBirthDate;
+
+	@FXML
+	private TextField txtBaseSalary;
+
+	@FXML
 	private Label labelErrorName;
+
+	@FXML
+	private Label labelErrorEmail;
+
+	@FXML
+	private Label labelErrorBirthDate;
+
+	@FXML
+	private Label labelErrorBaseSalary;
 
 	@FXML
 	private Button btSave;
@@ -147,6 +169,12 @@ public class SellerFormController implements Initializable {
 		txtId.setText(String.valueOf(entity.getId()));// setando o valor do id no campo id/String.valueOf faz a
 													  // conversão pro text receber
 		txtName.setText(entity.getName());// setando o valor do id no campo name
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if(entity.getBirthDate() !=null) {
+		dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
 
 	public SellerFormController() {
@@ -163,7 +191,10 @@ public class SellerFormController implements Initializable {
 	 */
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);//
-		Constraints.setTextFieldMaxLength(txtName, 15);
+		Constraints.setTextFieldMaxLength(txtName, 50);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyy");
 	}
 
 }
